ce07cc6791e8cba2ecb6a91145a4cc1172fd6322
zhuzemin <zhuzemin4@hotmail.com>
2021-01-15 07:20:08 +0800

    Update 5278_display_m3u8_address.user.js
    fixed and improve preformance

--- 5278_display_m3u8_address.user.js
+++ 5278_display_m3u8_address.user.js
@@ -6,81 +6,83 @@
 // @description 5278 show m3u8 address
 // @description:zh-CN 5278 show m3u8 address
 // @description:zh-TW 5278 show m3u8 address
-// @include     http://www.5278.cc/forum.php?mod=viewthread&tid=*
-// @version     1.0
-// @grant       GM_xmlhttpRequest
-// @run-at      document-start
+// @include     https://5278.cc/thread-*-1-1.html
+// @include     https://hbo6.hboav.com/v4/public/Player.php?key=*
+// @version     1.01
+// @run-at      document-end
 // @author      zhuzemin
 // @license     Mozilla Public License 2.0; http://www.mozilla.org/MPL/2.0/
 // @license     CC Attribution-ShareAlike 4.0 International; http://creativecommons.org/licenses/by-sa/4.0/
-// @connect-src video1.yocoolnet.in
+// @grant         GM_registerMenuCommand
+// @grant         GM_setValue
+// @grant         GM_getValue
 // ==/UserScript==
-var config = {
-  'debug': false
+let config = {
+  'debug': true,
+  'version': GM_getValue('version')||'2.9.1'
 };
-var debug = config.debug ? console.log.bind(console)  : function () {
+let debug = config.debug ? console.log.bind(console) : function () {
 };
-class Player{
-  constructor(href) {
-    this.method = 'GET';
-    this.url = href;
-    this.headers = {
-      'User-agent': 'Mozilla/4.0 (compatible) Greasemonkey',
-      'Accept': 'application/atom+xml,application/xml,text/xml',
-      'Referer': window.location.href,
-    };
-    this.charset = 'text/plain;charset=utf8';
+// prepare UserPrefs
+setUserPref(
+  'version',
+  config.version,
+  'Set N_m3u8DL version',
+  `Set N_m3u8DL version`,
+);
+let init = function () {
+  if (window.self === window.top) {
+    let input = document.createElement("input");
+    input.setAttribute("type", "text");
+    input.size = window.screen.width;
+    document.body.insertBefore(input, document.body.firstChild);
+    let N_m3u8DL = document.createElement("input");
+    N_m3u8DL.setAttribute("type", "text");
+    N_m3u8DL.size = window.screen.width;
+    document.body.insertBefore(N_m3u8DL, document.body.firstChild);
+    window.addEventListener('message', function (e) {
+      debug(e.data);
+      if (e.data.includes('hboav.com')) {
+        let title = document.title.replace(' 成人線上直播一區 5278 / 5278論壇 / 我愛78論壇"', '');
+        let iframe = document.querySelector("iframe.cc5278_player");
+        let src = iframe.getAttribute("src");
+        debug(src);
+        N_m3u8DL.setAttribute("value", 'N_m3u8DL-CLI_v' + config.version + ' "' + e.data + '" --headers "Referer:' + src + '"  --saveName "' + title + '"');
+        input.setAttribute("value", e.data);
+      }
+    });
   }
-};
-var getLocation = function(href) {
-    var l = document.createElement("a");
-    l.href = href;
-    return l;
-};
-var init = function () {
-	var allmyplayer=document.querySelector("#allmyplayer");
-	var src=allmyplayer.getAttribute("src");
-      debug(src);
-      var player = new Player(src);
-      debug(player.url);
-      GM_xmlhttpRequest({
-        method: player.method,
-        url: player.url,
-        headers: player.headers,
-        overrideMimeType: player.charset,
-        //synchronous: true
-        onload: function (responseDetails) {
-          debug(responseDetails);
-          var Html = new DOMParser().parseFromString(responseDetails.responseText, "text/html");
-          debug(Html);
-          var m3u8;
-          var p1 = Html.querySelector('#payer');
-          if(p1!=null){
-            var script=p1.querySelector("script");
-            var url=script.innerText.match(/player\.src\('([:\/\.\d\w]*)'\)/)[1];
-            //var hostname=getLocation(player.url).hostname;
-            m3u8=url;
-          }
-          else{
-            var mediaplayer1=Html.querySelector("#mediaplayer1");
-            var script=mediaplayer1.nextElementSibling;
-            var url=script.innerText.match(/url:\s"([:\/\.\d\w]*)"/)[1];
-            m3u8=url;
-          }
-          debug(m3u8);
-          var title=document.title.replace(' 成人線上直播一區 5278 / 5278論壇 / 我愛78論壇"','');
-          var input=document.createElement("input");
-          input.setAttribute("type","text");
-          input.setAttribute("value",m3u8);
-          input.size=100;
-          document.body.insertBefore(input,document.body.firstChild);
-          var N_m3u8DL=document.createElement("input");
-          N_m3u8DL.setAttribute("type","text");
-          N_m3u8DL.setAttribute("value",'N_m3u8DL-CLI_v2.4.6 "'+m3u8+'" --headers "Referer:https://video1.yocoolnet.in/|Accept:text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"  --saveName "'+title+'"');
-          N_m3u8DL.size=100;
-          document.body.insertBefore(N_m3u8DL,document.body.firstChild);
-        }
-      });
+  else {
+    let video = document.querySelector('video');
+    let interval = setInterval(() => {
+      let script = video.querySelector('script');
+      if (script) {
+        //debug(script.innerHTML);
+        //let url = script.innerHTML.match(/ /)[1];
+        let url = unsafeWindow.player.src();
+        debug(url);
+        parent.postMessage(url, "*");
+        clearInterval(interval);
+      }
+    }, 1000);
+  }
 }
 window.addEventListener('DOMContentLoaded', init);
-	
+/**
+ * Create a user setting prompt
+ * @param {string} varName
+ * @param {any} defaultVal
+ * @param {string} menuText
+ * @param {string} promtText
+ * @param {function} func
+ */
+function setUserPref(varName, defaultVal, menuText, promtText, func = null) {
+  GM_registerMenuCommand(menuText, function () {
+    var val = prompt(promtText, GM_getValue(varName, defaultVal));
+    if (val === null) { return; }  // end execution if clicked CANCEL
+    GM_setValue(varName, val);
+    if (func != null) {
+      func(val);
+    }
+  });
+}
